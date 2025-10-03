38f372f1f1abccb0f278100c440812bb5f8bd4b5
rafaelmm16 <rafael.merlomendes@gmail.com>
2025-10-03 07:46:04 -0300

    fix: corrigir configuração de basePath e assetPrefix no next.config.js

--- tailwind-spotify/next.config.js
+++ tailwind-spotify/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/spotify-cover',
+  assetPrefix: '/spotify-cover',
   images: {
     unoptimized: true,
     remotePatterns: [
